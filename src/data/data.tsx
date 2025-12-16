9acd5be307cfb271bf98c08cfc051af071477c1c
Kuldeep Khalotiya <kuldeepkhalotiya5@gmail.com>
2025-12-16 13:47:55 +0530

    Fix resume download link and add resume PDF

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -72,7 +72,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      href: '/resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
